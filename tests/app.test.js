e46f401eb802ff673c633510d2d7ceb5bbd6b877
Dennis <dennisdiepolder@outlook.com>
2024-03-05 13:06:03 +0100

    fixed: timeout problem

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,24 +1,27 @@
-const { By, Builder, Key } = require("selenium-webdriver")
-const assert = require("assert")
+const { By, Builder, Key, until } = require("selenium-webdriver");
+const assert = require("assert");
 
 describe("add note", function() {
     it("should add a note and display on the page", async function() {
         // Testlogik hier
-        // hier kommt Silneium
+        // hier kommt Selenium
         let driver = await new Builder().forBrowser('chrome').build();
         try {
             await driver.get('http://localhost:3000');
             await driver.findElement(By.xpath('//input')).sendKeys('Hello Selenium', Key.RETURN);
 
+            // Warte, bis die Note angezeigt wird
+            await driver.wait(until.elementLocated(By.xpath("//*[@id='root']/div/div")), 5000);
+
             let note = await driver.findElement(By.xpath("//*[@id='root']/div/div"))
-                                   .getText()
-            console.log({ note })
-            assert.equal(note, 'Hello Selenium\nX')
-            console.log("TEST PASSED!")
+                                   .getText();
+            console.log({ note });
+            assert.equal(note, 'Hello Selenium\nX');
+            console.log("TEST PASSED!");
         } catch (e) {
-            console.error(e)
+            console.error(e);
         } finally {
             await driver.quit();
         }
-    })
-})
+    });
+});
