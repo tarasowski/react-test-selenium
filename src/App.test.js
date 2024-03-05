bfa758d8eb9849055b3bebdb88430a143b66469f
arberatD <arberatdushi@gmail.com>
2024-03-05 13:28:52 +0100

    another try

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const assert = require('assert');
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+let options = new chrome.Options();
+options.addArguments(
+    'headless', 
+    'no-sandbox', 
+    'disable-dev-shm-usage' 
+);
++
+describe("add note", function() {
+    it("should add a note and display on the page", async function() {
+        // Testlogik hier
+        // hier kommt Silneium
+        let driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
+        try {
+            await driver.get('http://localhost:3000');
+            await driver.findElement(By.xpath('//input')).sendKeys('Hello Selenium', Key.RETURN);
+
+            let note = await driver.findElement(By.xpath("//*[@id='root']/div/div"))
+                                   .getText()
+            console.log({ note })
+            assert.equal(note, 'Hello Selenium\nX')
+            console.log("TEST PASSED!")
+        } catch (e) {
+            console.error(e)
+        } finally {
+            await driver.quit();
+        }
+    })
+})
